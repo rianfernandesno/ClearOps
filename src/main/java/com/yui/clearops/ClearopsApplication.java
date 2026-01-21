dbf5a11118f125fbced89b06e60e1b90782e058a
rianfernandesno <zrpapz12@gmail.com>
2026-01-21 08:05:14 -0300

    "Start Project"

---
+++ src/main/java/com/yui/clearops/ClearopsApplication.java
@@ -0,0 +1,13 @@
+package com.yui.clearops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yui/clearops/ClearopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yui.clearops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
